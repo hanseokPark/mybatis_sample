a6f5f87c1b27de15873e7c7cfde89702ee15cde8
hanseok Park <es12850@naver.com>
2018-02-12 10:18:44 +0900

    SelectStudentByAllWithAPIForMap 추가

--- src/main/java/kr/or/dgit/mybatis_sample/service/StudentService.java
+++ src/main/java/kr/or/dgit/mybatis_sample/service/StudentService.java
@@ -1,11 +1,13 @@
 package kr.or.dgit.mybatis_sample.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
 
+import kr.or.dgit.mybatis_sample.dao.StudentDao;
 import kr.or.dgit.mybatis_sample.dto.Student;
 import kr.or.dgit.mybatis_study_util.MyBatisSqlSessionFactory;
 
@@ -56,6 +58,13 @@ public class StudentService {
 		log.debug("selectStudentByAllWithAPIForResultMap()");
 		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
 			return sqlSession.selectList(namespace + "selectStudentByAllWithAPIForResultMap");
+		}
+	}
+	public List<Map<String, Object>> selectStudentByAllWithAPIForMap() {
+		log.debug("selectStudentByAllWithAPIForMap()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+			return studentDao.selectStudentByAllWithAPIForMap();
 		}
 	}
 }
