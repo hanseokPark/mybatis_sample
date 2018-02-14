188a2fa9d5a60fe2c2a3696f7c17e1ad81e61e9d
hanseok Park <es12850@naver.com>
2018-02-14 10:59:26 +0900

    row바운드 API

---
+++ src/main/java/kr/or/dgit/mybatis_sample/service/AddressService.java
@@ -0,0 +1,23 @@
+package kr.or.dgit.mybatis_sample.service;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.mybatis_sample.dto.Address;
+import kr.or.dgit.mybatis_study_util.MyBatisSqlSessionFactory;
+
+public class AddressService {
+	private static final Log log = LogFactory.getLog(AddressService.class);
+	private String namespace = "kr.or.dgit.mybatis_sample.dao.AddressDao.";
+	
+	public List<Address> selectAddressByAllWithAPI(RowBounds rowBounds) {
+		log.debug("selectAddressByAllWithAPI()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectList(namespace + "selectAddressByAllWithAPI", null, rowBounds);
+		}
+	}
+}
